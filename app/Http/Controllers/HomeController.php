08b7966c7dc92ce72031aaf4749aab8b35cce58c
hanan3889 <rebaia_hanan@yahoo.fr>
2024-09-18 11:37:15 +0200

    add API to project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use App\Models\Recipe;
 use App\Models\Category;
 
@@ -12,22 +10,39 @@ class HomeController extends Controller
 
     public function index()
     {
-        $recipes = Recipe::where('isactive', 1)
-                 ->orderBy('created_at', 'desc')
-                 ->take(10)
-                 ->get();
-
+        $recipes = Recipe::orderBy('created_at', 'desc')->get();
         $categories = Category::where('isActive', 1)->orderBy('created_at', 'DESC')->with('recipes')->get();
-        
-        return view('front.app', compact('recipes', 'categories'));
+
+        if (request()->wantsJson()) {
+            $recipes->each(function ($recipe) {
+                if ($recipe->image) {
+                    $recipe->image = url('storage/' . $recipe->image);
+                }
+            });
+            return response()->json([
+                'recipes' => $recipes,
+                'categories' => $categories
+            ], 200);
+        }
+        else {
+            return view('front.app', compact('recipes', 'categories'));
+        }
     }
 
     public function show($id)
     {
-        // Récupérer la recette avec ses relations author et category
         $recipe = Recipe::with(['author', 'category'])->findOrFail($id);
 
-        // Retourner la vue avec la recette spécifique
-        return view('front.show', compact('recipe'));
+        if (request()->wantsJson()) {
+            $recipe->each(function ($recipe) {
+                if ($recipe->image) {
+                    $recipe->image = url('storage/' . $recipe->image);
+                }
+            });
+            return response()->json($recipe, 200);
+        }
+        else {
+            return view('front.show', compact('recipe'));
+        }
     }
 }
---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->configureRateLimiting();
+
+        $this->routes(function () {
+            // Load the "api.php" routes with "api" middleware and "api" prefix
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            // Load the "web.php" routes with "web" middleware
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+
+    protected function mapApiRoutes()
+{
+    Route::prefix('api')
+        ->middleware('api')
+        ->namespace($this->namespace)
+        ->group(base_path('routes/api.php'));
+}
+}
